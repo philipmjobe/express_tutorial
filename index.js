f715dbff388369d9894562183784f557852c5704
Philip <philipmjobe@gmail.com>
2023-03-03 13:24:18 -0500

    tests http endpoints

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function (req, res) {
+app.get("/hello", function (req, res) {
   res.send("hello world");
 });
 
+app.post("/hello", function (req, res) {
+  res.send("you just called the post method at '/hello'!\n");
+});
+
+app.all("/test", function (req, res) {
+  res.send("HTTP method doesn't have any effect of this route!");
+});
 app.listen(3000);
