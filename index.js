acf85069c543b948fa0ea4a0d6f9b308be4679e9
Philip <philipmjobe@gmail.com>
2023-03-03 14:21:25 -0500

    writes first routes

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.get("/hello", function (req, res) {
-  res.send("hello world");
-});
+var things = require("./things.js");
 
-app.post("/hello", function (req, res) {
-  res.send("you just called the post method at '/hello'!\n");
-});
-
-app.all("/test", function (req, res) {
-  res.send("HTTP method doesn't have any effect of this route!");
-});
+app.use("/things", things);
 app.listen(3000);
