5093b9c52da1acbe1a3716661e9cff2f2e8fdd80
Philip <philipmjobe@gmail.com>
2023-03-03 14:29:30 -0500

    installs pug

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require("express");
 var app = express();
+app.set("view engine", "pug");
+app.set("views", "./views");
 
 var things = require("./things.js");
 
 app.use("/things", things);
+
+app.get("/first_template", function (req, res) {
+  res.render("first_view");
+});
 app.listen(3000);
